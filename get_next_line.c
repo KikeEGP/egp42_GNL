b82c35b785aafe827617130d9c24cde044b15a84
Enrique Gil Piqueras <enrgil-p@c1r7s2.42madrid.com>
2024-08-03 22:48:53 +0200

    Thought about how to keep readed in line and aux vars, I need to think more about it

--- get_next_line.c
+++ get_next_line.c
@@ -6,20 +6,34 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 18:30:21 by enrgil-p          #+#    #+#             */
-/*   Updated: 2024/08/03 20:05:39 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2024/08/03 22:47:07 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_nextline.h"
+#include "get_next_line.h"
+
+static	end_line(const char *s) //As strchr, but just for '\n'
+{
+	while (*s)
+	{
+		if (*s == '\n')
+			return ((char *)s);
+		s++;
+	}
+	return (0)
+}
 
 char	*get_next_line(int fd)
 {
 	char	*line; //May you need to allocate mem here?
 		       //How many? Maybe, BUFFER_SIZE plus BUF_SIZE...
 		       //until you don't reach \n?
+	char	aux[BUFFER_SIZE + 1];
 	char	*buf;
 	ssize_t	nb_read;
-	
+	int	i;
+
+	i = 1;		//index to count times read() is called
 	while (nb_read != 0)
 	{
 		nb_read = read (fd, buf, BUFFER_SIZE); //May I protect
@@ -39,6 +53,17 @@ char	*get_next_line(int fd)
 		//Before you save the buffer in heap, check it wth strchr
 		//If there's no \n, copy complete. If you find \n,
 		//copy just to \n, an keep next chars for next line
+		if (!end_line(buf))
+		{
+		
+			aux = strdup(line);
+			line = (char *)malloc(i * BUFFER_SIZE + 1);
+			line = memcpy(line, aux, strlen(aux));
+			//How to do the copy? Strlcpy, memcpy, what?
+			//Do I have memory allocated for this?
+			//How to manage empty vars at the begining???????
+		}
+		i++;
 	}
 	return (line);
 }
