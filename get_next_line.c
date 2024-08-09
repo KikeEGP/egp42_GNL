23693275260a32875388497ade1e35c9343d0ac8
Enrique Gil Piqueras <enrgil-p@c1r11s1.42madrid.com>
2024-08-09 20:54:45 +0200

    Start preparing utils functions and all needed to save line. Didn't started with pointer for next read

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 18:30:21 by enrgil-p          #+#    #+#             */
-/*   Updated: 2024/08/08 21:51:24 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2024/08/09 20:50:05 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,69 +17,67 @@ static char	*end_line(const char *s) /*As strchr, but just for '\n'*/
 	while (*s)
 	{
 		if (*s == '\n')
-			return ((unsigned char *)s+1);
-					//If finds end of line, will
-					//return pointer to new line
+			return ((unsigned char *)s);
 		s++;
 	}
 	return (0);
 }
 
+static char	*line_returned(char *aux)/*It's like substr, 
+					  to get the complete line*/
+{
+	char	*the_line;
+	char	*end;
+	size_t	len;
+
+	if (aux)
+	{
+		end = end_line(aux);
+		len = strlen_gnl(aux) - strlen_gnl(end);
+		the_line = (char *)malloc((len + 1) * sizeof(char));
+		the_line = memcpy_line(the_line, aux, len);
+		if (!the_line)
+			return (NULL);
+		the_line[len] = '\0';
+	}
+	return (the_line);
+}
+
 char	*get_next_line(int fd)
 {
-	char		*line; 
-	char		aux;
-	static	char	*buf;
-	ssize_t		nb_read;
+	char	*line;
+	char	aux;
+	char	*buf;
+	ssize_t	nb_read;
 
-	buf = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!buf)
-		return (0);
-		//Don't forget to asign '\0'
-		//
-	nb_read = -1;
-	while (nb_read != 0) //Maybe this while must be until I don't find
-			     //the \n, because I don't have to reach
-			     //end of line. Maybe I need to be prepared
-			     //for both cases, though
+	while (/*????????????*/)/*LOOP MUST STOP AFTER end_line(buf)!!*/
 	{
 		nb_read = read(fd, buf, BUFFER_SIZE); //May I protect
 						       //the BUFFER_SIZE
 						       //from SSIZE_MAX value
-		if (nb_read < 0)
+		if (nb_read <= 0)
 			return (NULL);
-		aux = strjoin(line, buf);
-		if (line)
-			free(line);
-		if (!end_line(buf))
-			line = strdup(aux);
+		buf[nb_read] = '\0';
+		aux = join_line(line, buf);
+		if (!aux)
+			return (NULL);
+		free(line);
 		if (end_line(buf))
 		{
-			end = end_line(buf);// REMOVE THIS LINE!!!
-					    // THIS FUNCTION MUST RUN WITH
-					    // FIRST CALL OR 25th CALL
-					    // SO BUFFER MUST BE ALWAYS
-					    // THE VAR WHICH LOCATES THE POINTER
-					    // TO READ FIRST
-			len = (strlen(line) - strlen(end));
-			line = substr(line, 0, len);
+			line = line_returned(aux);
 			free(/*WATCH OUT, SOMETHINNG MUST BE FREE
 			OR TAKE LESS MEMORY INSTEAD*/)
-			if (!line)
-			{
-				free(line);
-				return (NULL);
-			}
 		}
-		
-		//So... have we reached the \n? 
-				  //You should check it
-			//Now I only want to store in line from buf[0]
-			//to buf[n] == end_line(buf). HOW?
-
+		else
+			line = dup_line(aux);//End of file or not found \n
+		//else
+		{
+			/*A FUNCTION TO CHECK IF MUST RETURN NULL AND/OR FREE*/
+			//free(line);
+			//return (NULL);
+		}
 	}
-	return (line);
-}
+	return	(line);}
 /*
 int	main(void)
 {
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,9 +6,70 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 18:29:37 by enrgil-p          #+#    #+#             */
-/*   Updated: 2024/08/04 16:44:55 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2024/08/09 20:52:22 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+strlen_gnl//like strlen
 
-strlen, memcpy, strdup, strjoin
+void	*memcpy_line(void *dest, const void *src, size_t len)//	like mempcy
+{
+	const char	*src_ptr;
+	char		*dest_ptr;
+	size_t		i;
+
+	i = 0;
+	if (!dest && !src)
+		return (NULL);
+	dest_ptr = (unsigned char *) dest;
+	src_ptr = (const unsigned char *) src;
+	while (i < len)
+	{
+		dest_ptr[i] = src_ptr[i];
+		i++;
+	}
+	return (dest);
+}
+
+char	*dup_line(const char *aux)//		like strdup
+{
+	char	*duplicate;
+	size_t	i;
+
+	duplicate = (char *)malloc(strlen_gnl(aux) + 1) * (sizeof(char));
+	if (!duplicate)
+		return (NULL);
+	i = 0;
+	while (aux[i])
+	{
+		duplicate[i] = aux[i];
+		i++;
+	}
+	duplicate[i] = '\0';
+	return (duplicate);
+}
+
+char	*join_line(char const *line, char const *buffer)//	like strjoin
+{
+	char	*aux;
+	size_t	len_line;
+	size_t	len_buffer;
+	size_t	len_joined;
+
+	if (!buffer)
+		return (NULL);
+	if (!line && buffer)
+		return (dup_line(buffer));
+	len_line = strlen_gnl(line);
+	len_buffer = strlen_gnl(buffer);
+	len_joined = len_line + len_buffer;
+	aux = (char *)malloc((len_joined + 1) * sizeof(char));
+	if (!aux)
+		return (NULL);
+	aux[len_joined] = '\0';
+	aux = memcpy_line(aux, line, len_line);
+	aux = memcpy_line(&aux[len_line], buffer, len_buffer);
+	while (len_line -- > 0)
+		aux--;
+	return (aux);
+}
