1c140fe1b584455d93a327c5bd7477bd9734b9c2
Enrique Gil Piqueras <enrgil-p@c1r5s1.42madrid.com>
2024-08-25 20:44:33 +0200

    Created bonus by copying regular function. Not changes yet. Remove main

---
+++ get_next_line_bonus.c
@@ -0,0 +1,134 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/03 18:30:21 by enrgil-p          #+#    #+#             */
+/*   Updated: 2024/08/25 20:41:17 by enrgil-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+/*					As strchr, but just for '\n'*/
+static char	*end_line(const char *s)
+{
+	while (*s)
+	{
+		if (*s == '\n')
+			return ((char *)s);
+		s++;
+	}
+	return (0);
+}
+
+/*If buffer has chars after \n, keep for next call. Free buf, buf != next*/
+static char	*keep_line(char *buf)
+{
+	char	*next;
+	char	*end;
+
+	end = end_line(buf);
+	if (end + 1)
+	{
+		next = dup_line(end + 1);
+		if (!next)
+			return (NULL);
+		free(buf);
+		return (next);
+	}
+	return (NULL);
+}
+
+/*				It's like substr to get the complete line*/
+static char	*line_returned(char *line)
+{
+	char	*the_line;
+	char	*end;
+	size_t	len;
+
+	if (line != NULL && end_line(line))
+	{
+		end = end_line(line);
+		len = strlen_gnl(line) - strlen_gnl(end);
+		the_line = (char *)malloc((len + 1) * sizeof(char));
+		if (!the_line)
+			return (NULL);
+		the_line[len] = '\0';
+		the_line = memcpy_line(the_line, line, len);
+		return (the_line);
+	}
+	return (NULL);
+}
+
+/*Read and save line unitl \n. buf null-end == !overflow; free line in join*/
+static char	*line_read(int fd, char *line)
+{
+	ssize_t	nb_read;
+	char	*buf;
+
+	buf = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buf)
+		return (NULL);
+	buf[BUFFER_SIZE] = '\0';
+	nb_read = 1;
+	while (!end_line(buf) && nb_read != 0)
+	{
+		nb_read = read(fd, buf, BUFFER_SIZE);
+		if (nb_read == -1)
+		{
+			free(buf);
+			free(line);
+			return (NULL);
+		}
+		buf[nb_read] = '\0';
+		line = join_line(line, buf);
+	}
+	free(buf);
+	return (line);
+}
+
+/*Three phases: read and save, prepare line and keep next line*/
+char	*get_next_line(int fd)
+{
+	static char	*line;
+	char		*ready;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (!line || !end_line(line))
+	{
+		line = line_read(fd, line);
+		if (!line)
+			return (NULL);
+	}
+	ready = line_returned(line);
+	if (!ready)
+	{
+		free(line);
+		free(ready);
+		return (NULL);
+	}
+	line = keep_line(line);
+	return (ready);
+}
+/*
+int	main(void)
+{
+	int	fd;
+	char	*lines;
+
+	fd = open("trying_read.txt", O_RDONLY);
+	lines = "";
+	while (lines != NULL)
+	{	
+		lines = get_next_line(fd);
+		//printf("--MAIN: lines <%s>\n", lines);
+		if (lines)
+			printf("**RESULT %s\n\n", lines);
+		free(lines);
+	}
+	return (0);
+}*/
---
+++ get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/02 19:06:49 by enrgil-p          #+#    #+#             */
+/*   Updated: 2024/08/25 20:37:54 by enrgil-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h> /*read()*/
+# include <fcntl.h> /*open()*/
+# include <stdlib.h> /*malloc and free*/
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+char	*get_next_line(int fd);
+/*	Part 2: functions at get_next_line_utils_bonus.c	*/
+size_t	strlen_gnl(const char *str);/*			normal	strlen	*/
+void	*memcpy_line(void *dest, const void *src, size_t len);/*memcpy	*/
+char	*dup_line(const char *aux);/*				strdup	*/
+char	*join_line(char *line, char *buffer);/*			strjoin	*/
+
+#endif
